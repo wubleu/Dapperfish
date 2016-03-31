9be817bd3fe0a408989cbab2e43463bb996414b1
Ryan Wilson <rwilson@oberlin.edu>
2016-03-31 15:14:09 -0400

    Branch

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+		player = new GameObject ();
+		player.AddComponent<PlayerController> ().init (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void MakeSprite(GameObject obj, Sprite s, Transform parentTransform, 
+						   float x, float y, float xScale, float yScale, float pixelsPer, params float[] pivot) {
+		obj.transform.parent = parentTransform;
+		obj.transform.localPosition = new Vector3 (x, y, 0);
+		obj.transform.localScale = new Vector3 (xScale, yScale, 1);
+		obj.name = s.name + "Sprite";
+		SpriteRenderer rend = obj.AddComponent<SpriteRenderer> ();
+		Sprite texture = s;
+		float xBound = .5f;
+		float yBound = .5f;
+		if (pivot.Length > 0) {
+			xBound = pivot [0];
+		} if (pivot.Length > 1) {
+			yBound = pivot [1];
+		}
+		rend.sprite = texture;
+	}
+}
---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// PARAMETERS
+	float speed = 2f;
+
+	GameManager parent;
+	Material playerMaterial;
+
+
+	// Use this for initialization
+	public void init (GameManager owner) {
+		parent = owner;
+		parent.MakeSprite (gameObject, Resources.Load<Sprite> ("Textures/Button"), transform, 0, -4, .25f, .25f, 200);
+		playerMaterial = GetComponent<SpriteRenderer> ().material;
+		playerMaterial.color = new Color (170f/256f, 0f/256f, 150f/256f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// moves player by speed*Time.deltaTime based on WASD
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (-speed*Time.deltaTime, 0, 0);
+		} if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (speed*Time.deltaTime, 0, 0);
+		} if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, speed*Time.deltaTime, 0);
+		} if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, -speed*Time.deltaTime, 0);
+		}
+	}
+}
