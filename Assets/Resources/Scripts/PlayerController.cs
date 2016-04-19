91e616c1a33c0f38c001db52af3925e76997ef80
Evan Lowy <elowy@oberlin.edu>
2016-04-18 23:33:33 -0400

    added necromancer model

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -5,11 +5,14 @@ using System.Collections.Generic;
 public class PlayerController : MonoBehaviour {
 
 	// PARAMETERS
-	public float hp = 500, mana = 100, recharge = 20, size = 0.3f;
+	public float hp = 500, mana = 100, recharge = 20, size = 1f;
 	float speed = 1.1f;
 	Color necroColor = new Color(120f / 256f, 0f / 256f, 100f / 256f);
 	float[] clocks = new float[5] {0, 0, 0, 0, 0}, timers = new float[5] {0.5f, 0.5f, 0.5f, 0.5f, 0.5f}, mcosts = new float[5] {0, 60, 30, 80, 30};
 	Melee melee;
+
+
+	protected Sprite[] cSprites;
 	// spellShotInterval = .2f
 
 	// CONTROLS PARAMETERS
@@ -28,12 +31,21 @@ public class PlayerController : MonoBehaviour {
 	public EnemyManager eManager;
 	public bool hasKey = false;
 
+	GameObject necromodel;
+	GameObject rightarm;
+	GameObject leftarm;
+	GameObject body;
+	SpriteRenderer lamodel;
+	SpriteRenderer bodymodel;
+	SpriteRenderer ramodel;
 
 	// Use this for initialization
 	public void init (GameManager owner, EnemyManager eMan) {
 		eManager = eMan;
 		gManager = owner;
 		gameObject.name = "Necromancer";
+
+		cSprites = Resources.LoadAll<Sprite> ("Textures/Necromancer Sprite Sheet 0");
 
 		gameObject.AddComponent<SphereCollider>().radius = size;
 		NavMeshAgent nav = gameObject.AddComponent<NavMeshAgent>();
@@ -46,15 +58,55 @@ public class PlayerController : MonoBehaviour {
 		rigid.freezeRotation = true;
 
 		// Necromancer Model
+		necromodel = new GameObject();
+		necromodel.transform.parent = transform;
+		necromodel.transform.localPosition = transform.position;
+
 		SpriteRenderer model = new GameObject().AddComponent<SpriteRenderer>();
 		model.name = "Necro Model";
-		model.transform.parent = transform;
+		model.transform.parent = necromodel.transform;
 		model.transform.localPosition = new Vector3(0, 1, 0);
-		model.transform.localEulerAngles = new Vector3(90, 0, 0);
+		model.transform.localEulerAngles = new Vector3(90, 180, 0);
 		model.transform.localScale = new Vector3(size, size, size);
 		model.sortingLayerName = "PlayerController";
-		model.sprite = Resources.Load<Sprite>("Textures/Circle");
-		model.color = necroColor;
+		model.sortingOrder = 4;
+		model.sprite = cSprites [9];
+
+		body = new GameObject ();
+		bodymodel = body.AddComponent<SpriteRenderer> ();
+		bodymodel.name = "Necro Body Model";
+		bodymodel.transform.parent = necromodel.transform;
+		bodymodel.transform.localPosition = new Vector3(0.03f, 1, 0.157f);
+		bodymodel.transform.localEulerAngles = new Vector3(90, 180, 0);
+		bodymodel.transform.localScale = new Vector3(size, size, size);
+		bodymodel.sortingLayerName = "PlayerController";
+		bodymodel.sortingOrder = 1;
+		bodymodel.sprite = cSprites [10];
+
+		leftarm = new GameObject ();
+		lamodel = leftarm.AddComponent<SpriteRenderer> ();
+		lamodel.name = "Left Arm Model";
+		lamodel.transform.parent = necromodel.transform;
+		lamodel.transform.localPosition = new Vector3(-0.136f, 1, 0.382f);
+		lamodel.transform.localEulerAngles = new Vector3(90, 180, 0);
+		lamodel.transform.localScale = new Vector3(size, size, size);
+		lamodel.sortingLayerName = "PlayerController";
+		lamodel.sortingOrder = 3;
+		lamodel.sprite = cSprites [14];
+
+		rightarm = new GameObject ();
+		ramodel = rightarm.AddComponent<SpriteRenderer> ();
+		ramodel.name = "Necro Right Arm Model";
+		ramodel.transform.parent = necromodel.transform;
+		ramodel.transform.localPosition = new Vector3(0.12f, 1, 0.29f);
+		ramodel.transform.localEulerAngles = new Vector3(90, 180, 0);
+		ramodel.transform.localScale = new Vector3(size, size, size);
+		ramodel.sortingLayerName = "PlayerController";
+		ramodel.sortingOrder = 2;
+		ramodel.sprite = cSprites [19];
+
+
+		//model.color = necroColor;
 
 		// HealthBar + ManaBar
 		new GameObject().AddComponent<HealthBar>().init(hp);
@@ -142,6 +194,11 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+	void LateUpdate(){
+		Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mouse.y = 0;
+		necromodel.transform.LookAt (mouse);
+	}
 
 	public void TakeHit(GameObject collObj) {
 		print ("taking hit");
