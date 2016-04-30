4dcd35e7f3f3c2036e09200c94c89c051774434d
Evan Lowy <elowy@oberlin.edu>
2016-04-30 17:22:52 -0400

    started adding story events

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -305,7 +305,7 @@ public class PlayerController : MonoBehaviour {
 	public void HasKey(){
 		if (!hasFortKey) {
 			hasFortKey = true;
-			gManager.ChangeObjective("Go to Fort to get gate key.");
+			gManager.ChangeObjective("Open the Fort gate.");
 		} else {
 			hasKey = true;
 			gManager.objectives.text = "Get through the East Gate. \nGate will take 3 seconds to open.";
