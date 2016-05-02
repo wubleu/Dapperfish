d289d358afd4465b2ed109a1cdefc494619ac346
Evan Lowy <elowy@oberlin.edu>
2016-05-02 16:09:28 -0400

    added level 2 gate

---
+++ Assets/Resources/Scripts/Gate2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gate2 : MonoBehaviour {
+
+	public GameManager Gman;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Gman.waveclear) {
+			Destroy (this.gameObject);
+		}
+	}
+}
