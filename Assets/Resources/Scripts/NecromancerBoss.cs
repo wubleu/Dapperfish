c9d09c2edb7d52c11f5c6779483c74c9578854dc
Evan Lowy <elowy@oberlin.edu>
2016-05-09 13:55:36 -0400

    fixed health

--- Assets/Resources/Scripts/NecromancerBoss.cs
+++ Assets/Resources/Scripts/NecromancerBoss.cs
@@ -5,7 +5,7 @@ public class NecromancerBoss : MonoBehaviour {
 
 	// PARAMETERS
 	float teleportCooldown = 3f, minSummonCooldown = 10f, maxSummonCooldown = 18f, summonRandomizeRange = 4f, 
-	damageCooldown = 20f, rootDuration = 1f, maxHp = 150f, aggroToAIRange = 4, shootingPeriod = 2f, shootingFreq = .26f, 
+	damageCooldown = 20f, rootDuration = 1f, maxHp = 180f, aggroToAIRange = 4, shootingPeriod = 2f, shootingFreq = .26f, 
 	shootingAngle = 30f, shootingPause = .6f, shootChance = 40, castleftcd = .5f, castrightcd = .5f, 
 	damageExplosionWait = .1f, damageExplosionCount = 5, damageExplosionTriggerIntervals = 49;
 
